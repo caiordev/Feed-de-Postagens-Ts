a1d569849b02145345a90cbefa0503a640514bec
caiordev <caioreisdev@gmail.com>
2023-08-13 18:32:24 -0300

    desfazendo o build

--- src/components/Avatar/Avatar.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from "./Avatar.module.css";
-export function Avatar({ hasBorder = true, ...props }) {
-    return (_jsx("img", { className: hasBorder ? styles.avatarWithBorder : styles.avatar, ...props }));
-}
--- src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from "./Header.module.css";
-export function Header() {
-    return (_jsx("header", { className: styles.header, children: _jsx("img", { src: "https://as2.ftcdn.net/v2/jpg/03/20/88/33/1000_F_320883393_q7ROQwEkFchU1WGGSfwxCG9X9CLUW0JD.jpg", alt: "Logotipo do ignite" }) }));
-}
